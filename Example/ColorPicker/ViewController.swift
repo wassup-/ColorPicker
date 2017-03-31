941e6e1f390121f1d06dbb6a9ca347122187219c
Tom Knapen <tom@appwise.be>
2017-03-31 17:37:14 +0200

    Initial commit

---
+++ Example/ColorPicker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Tom Knapen on 03/31/2017.
+//  Copyright (c) 2017 Tom Knapen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
